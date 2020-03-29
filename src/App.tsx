ec9ddb4a9872a5ed470c47e382b765c298187ef7
Maxime Antoine <maximeantoine1997@gmail.com>
2020-03-29 18:06:53 +0200

    Update layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,45 @@
+import { createStyles, Grid, makeStyles } from "@material-ui/core";
+import SideBar from "Components/Navigation/Sidebar";
 import React from "react";
-import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import MyStocks from "Pages/MyStocks";
+import AddStocks from "Pages/AddStocks";
+import HomePage from "Pages/HomePage";
 
-function App() {
-  return (
-    <Grid>
-      <Drawer></Drawer>
-    </Grid>
-  );
-}
+const useStyles = makeStyles(() =>
+   createStyles({
+      content: {
+         marginLeft: "10vw",
+         width: "90vw",
+         height: "100vh",
+         background: "linear-gradient(to right, #182035, #131A2E);",
+         color: "white",
+      },
+   })
+);
+
+const App: React.FC = () => {
+   const classes = useStyles();
+   return (
+      <Router>
+         <Grid direction="row" container>
+            <SideBar />
+            <Grid item xs={12} justify="center" className={classes.content}>
+               <Switch>
+                  <Route path="/my-stocks">
+                     <MyStocks />
+                  </Route>
+                  <Route path="/add-stocks">
+                     <AddStocks />
+                  </Route>
+                  <Route exact path="/">
+                     <HomePage />
+                  </Route>
+               </Switch>
+            </Grid>
+         </Grid>
+      </Router>
+   );
+};
 
 export default App;
